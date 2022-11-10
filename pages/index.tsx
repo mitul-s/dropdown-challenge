59f810f9ecf1b53245214f857b1154dad6a674d5
Mitul Shah <mitulxshah@gmail.com>
2022-11-09 20:23:57 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,10 @@ import {
   PopoverContent,
   PopoverPortal,
 } from "@radix-ui/react-popover";
-import {
-  ScrollArea,
-  ScrollAreaViewport,
-  ScrollAreaScrollbar,
-  ScrollAreaThumb,
-} from "@radix-ui/react-scroll-area";
 import useKeyPress from "../hooks/useKeyPress";
 import { useReducer } from "react";
 import SearchIcon from "../components/SearchIcon";
-import { currencies as data } from "../lib/cryptocurrencies";
+import { currencies as data } from "../consts";
 
 interface Dropdown {
   useSearch: boolean;
@@ -61,8 +55,6 @@ function reducer(state: State, action: Action) {
             : state.options.length - 1,
       };
     case "arrowDown":
-      console.log(state.selectedIndex);
-      console.log("options", state.options.length - 1);
       return {
         options: state.options,
         selectedIndex:
@@ -89,6 +81,12 @@ const Option = ({
   onMouseMove,
   onClick,
 }: Option) => {
+  const formatCurrency = (currency: number) =>
+    new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(currency);
+
   return (
     <li
       className="list-none transition rounded data-[selected=true]:bg-gray-light group flex items-center w-full h-full p-2 text-xs leading-none gap-x-3 cursor-pointer"
@@ -99,15 +97,9 @@ const Option = ({
       onClick={onClick}
       onMouseMove={onMouseMove}
     >
-      <Image
-        src={image}
-        className="w-5 h-5 rounded-full"
-        alt={`Logo for ${title} currency`}
-      />
+      <Image src={image} className="w-5 h-5 rounded-full" alt="" />
       <div className="flex flex-col items-start gap-y-1">
-        <div className="w-24 font-medium overflow-x-clip text-ellipsis whitespace-nowrap">
-          {title}
-        </div>
+        <div className="font-medium">{title}</div>
         {subtitle && (
           <span className="tracking-tight uppercase text-black/50">
             ${subtitle}
@@ -118,11 +110,7 @@ const Option = ({
         <div className="invisible px-1.5 py-0.5 leading-none transition rounded bg-gray text-gray-dark group-data-[selected=true]:visible">
           Enter ↵
         </div>
-        {descriptor &&
-          new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD",
-          }).format(descriptor)}
+        {descriptor && formatCurrency(descriptor)}
       </div>
     </li>
   );
@@ -131,7 +119,7 @@ const Option = ({
 const Dropdown = ({ useSearch, options }: Dropdown) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
   const containerRef = React.useRef<HTMLUListElement>(null);
-  const [searchValue, setSearchValue] = React.useState<string>("");
+  const [searchValue, setSearchValue] = React.useState("");
   const [filter, setFilter] = React.useState({
     query: "",
     list: options,
@@ -141,18 +129,11 @@ const Dropdown = ({ useSearch, options }: Dropdown) => {
   const arrowDownPressed = useKeyPress("ArrowDown", inputRef);
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handleOpen = () => {
-    if (!open) {
-      if (inputRef.current?.value !== "") {
-        inputRef.current?.select();
-      }
-      setOpen(true);
-    }
-  };
-
   const [open, setOpen] = React.useState<boolean>(false);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    handleOpen();
+    if (!open) {
+      setOpen(true);
+    }
     setSearchValue(e.target.value);
 
     const results = options.filter((item) => {
@@ -170,8 +151,8 @@ const Dropdown = ({ useSearch, options }: Dropdown) => {
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (!open && (e.key === "ArrowDown" || e.key === "ArrowUp")) {
-      handleOpen();
+    if (!open && e.key === "ArrowDown") {
+      setOpen(true);
     }
 
     if (e.key === "Enter") {
@@ -182,26 +163,14 @@ const Dropdown = ({ useSearch, options }: Dropdown) => {
     if (e.key === "ArrowDown" || e.key === "ArrowUp") {
       e.preventDefault();
     }
-    // if (!e.isDefaultPrevented()) {
-    //   const container = containerRef.current;
-    //   if (!container) return;
+  };
 
-    //   window.requestAnimationFrame(() => {
-    //     const element = container.querySelector(
-    //       "[aria-selected=true]"
-    //     ) as HTMLElement;
-    //     if (element) {
-    //       const top = element.offsetTop - container.scrollTop;
-    //       const bottom =
-    //         container.scrollTop +
-    //         container.clientHeight -
-    //         (element.offsetTop + element.clientHeight);
-
-    //       if (bottom < 0) container.scrollTop -= bottom;
-    //       if (top < 0) container.scrollTop += top;
-    //     }
-    //   });
-    // }
+  const handleClick = () => {
+    // select should happen on any opening interaction, not just click
+    if (inputRef.current?.value !== "") {
+      inputRef.current?.select();
+    }
+    setOpen(true);
   };
 
   const handleSelect = (title: string) => {
@@ -226,13 +195,7 @@ const Dropdown = ({ useSearch, options }: Dropdown) => {
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverAnchor>
         {useSearch ? (
-          <div
-            className="relative transition-all rounded overflow-clip border gap-x-1.5 leading-none w-[346px] focus-within:ring-2 focus-within:ring-offset-2 hover:border-blue-200 focus-within:border-blue-200 group"
-            role="combobox"
-            aria-controls="cmpd-currencies"
-            aria-expanded={open}
-            aria-haspopup="listbox"
-          >
+          <div className="relative transition-all rounded overflow-clip border gap-x-1.5 leading-none w-[346px] focus-within:ring-2 focus-within:ring-offset-2 hover:border-blue-200 focus-within:border-blue-200 group">
             <label className="sr-only" id="cmpd-label" htmlFor="assetSearch">
               Search for assets
             </label>
@@ -240,17 +203,17 @@ const Dropdown = ({ useSearch, options }: Dropdown) => {
               className="py-3.5 px-4 outline-none w-full transition group-hover:opacity-80 group-focus-within:opacity-80 opacity-60"
               placeholder="Search for an asset"
               id="assetSearch"
+              role="combobox"
+              aria-controls="cmpd-currencies"
+              aria-expanded={open}
               aria-labelledby="cmpd-label"
-              aria-autocomplete="list"
               autoCorrect="off"
               autoComplete="off"
-              role="searchbox"
               spellCheck={false}
               type="text"
               value={searchValue}
               onChange={handleChange}
-              onClick={handleOpen}
-              // onFocus={handleOpen}
+              onClick={handleClick}
               onKeyDown={handleKeyDown}
               ref={inputRef}
             />
@@ -265,7 +228,6 @@ const Dropdown = ({ useSearch, options }: Dropdown) => {
           <button
             className="w-full py-2 text-white transition border rounded shadow-sm bg-gray-darker hover:bg-gray-darker/90 active:shadow-none hover:shadow"
             onClick={() => setOpen(!open)}
-            // ref={inputRef}
           >
             Search
           </button>
@@ -274,45 +236,39 @@ const Dropdown = ({ useSearch, options }: Dropdown) => {
       <PopoverPortal>
         <PopoverContent
           onOpenAutoFocus={(e) => e.preventDefault()}
-          className="w-[346px] px-2 bg-white border rounded shadow-xl popover overflow-hidden"
+          className="w-[346px]"
           id="cmpd-currencies"
           sideOffset={8}
-          asChild
         >
-          <ScrollArea>
-            <ScrollAreaViewport className="w-full h-full rounded-inherit">
-              <ul className="py-1.5" role="listbox" ref={containerRef}>
-                {filter.list.map((item, index) => {
-                  return (
-                    <Option
-                      key={item.title}
-                      selected={state.selectedIndex === index}
-                      onMouseMove={() =>
-                        dispatch({
-                          type: "select",
-                          payload: {
-                            selectedIndex: index,
-                            options: filter.list,
-                          },
-                        })
-                      }
-                      onClick={() => handleSelect(item.title)}
-                      {...item}
-                    />
-                  );
-                })}
-                {/* {searchValue !== "" && filter.list.length === 0 ? (
-                  <div>No results found!</div>
-                ) : null} */}
-              </ul>
-            </ScrollAreaViewport>
-            <ScrollAreaScrollbar
-              className="flex bg-white select-none touch-none p-1 data-[orientation=vertical]:w-2"
-              orientation="vertical"
-            >
-              <ScrollAreaThumb className="relative flex-1 w-4 transition rounded-md bg-gray-dark/50 hover:bg-gray-dark" />
-            </ScrollAreaScrollbar>
-          </ScrollArea>
+          <ul
+            className="flex flex-col px-2 overflow-auto bg-white border rounded shadow-xl h-fit max-h-64 py-1.5"
+            role="listbox"
+            ref={containerRef}
+          >
+            {filter.list.map((item, index) => {
+              // console.log(filter.list);
+              return (
+                <Option
+                  key={item.title}
+                  selected={state.selectedIndex === index}
+                  onMouseMove={() =>
+                    dispatch({
+                      type: "select",
+                      payload: {
+                        selectedIndex: index,
+                        options: filter.list,
+                      },
+                    })
+                  }
+                  onClick={() => handleSelect(item.title)}
+                  {...item}
+                />
+              );
+            })}
+            {/* {searchValue !== "" && filter.list.length <= 2 ? (
+              <Option title={`Add "${searchValue}" manually`} />
+            ) : null} */}
+          </ul>
         </PopoverContent>
       </PopoverPortal>
     </Popover>
@@ -323,7 +279,7 @@ export default function Home() {
   return (
     <div className="grid w-screen h-screen gap-y-12 place-content-center">
       <Dropdown useSearch={true} options={data} />
-      <Dropdown useSearch={false} options={data} />
+      {/* <Dropdown useSearch={false} options={data} /> */}
     </div>
   );
 }
