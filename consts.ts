cb8299d02076887252bb31fedfff5f3e99c084d8
Mitul Shah <mitulxshah@gmail.com>
2022-11-09 22:01:33 -0500

    Update consts.ts

--- consts.ts
+++ consts.ts
@@ -1,11 +1,13 @@
-import bitcoin from "./public/images/bitcoin.png";
-import eth from "./public/images/ethereum.png";
-import ltc from "./public/images/litecoin.png";
-import omg from "./public/images/omg-network.png";
-import usdc from "./public/images/usdc.png";
-import bch from "./public/images/bitcoin-cash.png";
-import dai from "./public/images/dai.png";
-import doge from "./public/images/doge-coin.png";
+import bitcoin from "../public/images/bitcoin.png";
+import eth from "../public/images/ethereum.png";
+import ltc from "../public/images/litecoin.png";
+import omg from "../public/images/omg-network.png";
+import usdc from "../public/images/usd-coin.png";
+import bch from "../public/images/bitcoin-cash.png";
+import xlm from "../public/images/stellar-lumens.png";
+import xrp from "../public/images/ripple.png";
+import dai from "../public/images/dai.png";
+import doge from "../public/images/doge-coin.png";
 
 export const currencies = [
   {
@@ -49,5 +51,33 @@ export const currencies = [
     subtitle: "LTC",
     descriptor: 3000,
     image: ltc,
+  },
+  {
+    id: 6,
+    title: "OMG Network",
+    subtitle: "OMG",
+    descriptor: 3000,
+    image: omg,
+  },
+  {
+    id: 7,
+    title: "United States Dollar Coin",
+    subtitle: "USDC",
+    descriptor: 3000,
+    image: usdc,
+  },
+  {
+    id: 8,
+    title: "Stellar Lumens",
+    subtitle: "XLM",
+    descriptor: 3000,
+    image: xlm,
+  },
+  {
+    id: 9,
+    title: "Ripple",
+    subtitle: "XRP",
+    descriptor: 3000,
+    image: xrp,
   },
 ];
